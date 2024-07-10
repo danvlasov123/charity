84ca028bd6c833cc825dbadcf517446c18030f05
Danil Vlasov <dan.vlasov123@gmail.com>
2024-07-10 11:29:09 +0300

    fix history

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -9,58 +9,12 @@ import { PAGES_PATH } from "src/router";
 import { fetchGetPayments } from "src/api/payments/payments";
 import { Loader } from "src/modules/Loader/Loader";
 
-const data = [
-  {
-    title: "AFRICA",
-    id: 1,
-    content:
-      "Important Message, Darlings Please take a minute to listen Important Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listen",
-    date: new Date(),
-    amount: 45,
-    success: false,
-  },
-  {
-    title: "AFRICA",
-    id: 2,
-    content:
-      "Important Message, Darlings Please take a minute to listen Important Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listen",
-    date: new Date(),
-    amount: 45,
-    success: false,
-  },
-  {
-    title: "AFRICA",
-    id: 3,
-    content:
-      "Important Message, Darlings Please take a minute to listen Important Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listen",
-    date: new Date(),
-    amount: 45,
-    success: true,
-  },
-  {
-    title: "AFRICA",
-    id: 4,
-    content:
-      "Important Message, Darlings Please take a minute to listen Important Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listen",
-    date: new Date(),
-    amount: 45,
-    success: true,
-  },
-  {
-    title: "AFRICA",
-    id: 5,
-    content:
-      "Important Message, Darlings Please take a minute to listen Important Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listenImportant Message, Darlings Please take a minute to listen",
-    date: new Date(),
-    amount: 45,
-    success: true,
-  },
-];
-
 const History = () => {
   const navigate = useNavigate();
 
   const [history, setHistory] = useState(null);
+
+  console.log({ history });
 
   const [opened, setOpened] = useState(null);
 
@@ -95,10 +49,10 @@ const History = () => {
 
   return (
     <div>
-      <div className="border-light-grey-2 flex justify-end border-b px-6 py-4">
+      <div className="pb flex justify-end border-b border-light-grey-2 px-6 py-4">
         <img src="/logo.svg" alt="logo" height={54} />
       </div>
-      <div className="px-6 pt-5">
+      <div className="px-6 pb-10 pt-5">
         <h1 className="text-xl font-medium leading-6">History</h1>
         <div className="flex flex-col gap-4 pt-4">
           {history.map((item) => {
@@ -116,7 +70,7 @@ const History = () => {
                 key={item.id}
                 className={className}
               >
-                <div className="bg-light-grey-2 rounded-md p-4">
+                <div className="rounded-md bg-light-grey-2 p-4">
                   <div className="flex cursor-pointer items-center justify-between">
                     <p>{item.title}</p>
                     <div className="flex items-center gap-2.5">
@@ -145,9 +99,11 @@ const History = () => {
                     <span>{format(item.date, "dd.MM.yyyy")}</span>
                   </div>
                   <div
-                    className={`text-green flex items-center justify-between pt-2.5 font-medium ${item.success ? "text-green" : "text-red"}`}
+                    className={`flex items-center justify-between pt-2.5 font-medium text-green ${item.status === "Approved" ? "text-green" : "text-red"}`}
                   >
-                    <p>{item.success ? "Approved" : "Declined"}</p>
+                    <p>
+                      {item.status === "Approved" ? "Approved" : "Declined"}
+                    </p>
                     <span>{item.amount}$</span>
                   </div>
                 </div>
